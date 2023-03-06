4be8f81a03ce8c948a5af434731b458355d16785
jjeongdong <donghun3296@naver.com>
2023-03-07 02:05:33 +0900

    0307

---
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,48 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hi!");
+        return "hello";     //resources/templates/hello.html을 찾아가라
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody   //http의 body부분에 return값을 직접 넣겠다
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody   //http 응답에 그대로 데이터를 넘김, 없으면 viewResolver에게 넘김
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;   //객체가 왔을 경우 JSON방식으로 데이터를 만들어 HTTP에 반환
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+}
