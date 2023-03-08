5a9025c98a9328e5af1b9276510682d5e780cf30
jjeongdong <donghun3296@naver.com>
2023-03-08 19:50:16 +0900

    0308

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,5 +1,6 @@
 package hello.hellospring;
 
+import hello.hellospring.aop.TimeTraceAop;
 import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
 import jakarta.persistence.EntityManager;
@@ -38,6 +39,10 @@ public class SpringConfig {
         return new MemberService(memberRepository);
     }
 
+//    @Bean
+//    public TimeTraceAop timeTraceAop() {
+//        return new TimeTraceAop();
+//    }
 //    @Bean
 //    public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();      //인터페이스는 new X
---
+++ src/main/java/hello/hellospring/aop/TimeTraceAop.java
@@ -0,0 +1,28 @@
+package hello.hellospring.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class TimeTraceAop {
+
+    @Around("execution(* hello.hellospring..*(..))")
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        System.out.println("START : " + joinPoint.toString());
+
+        try {
+//            Object result = joinPoint.proceed();
+//            return result;
+            return joinPoint.proceed();     //AOP적용 후, 가짜 서비스(프록시)를 생성 -> joinPoint.proceed() -> 진짜 서비스 호출
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("END : " + joinPoint.toString() + " " + timeMs + "ms");
+
+        }
+    }
+}
